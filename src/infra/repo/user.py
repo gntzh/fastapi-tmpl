1262bbbb1ea3567c2d763a26fa4874f2f596ef70
Grant Zhang <grant.zhang.s@outlook.com>
2021-10-07 13:45:02 +0800

    fix: change typing's Session to AsyncSession

--- src/infra/repo/user.py
+++ src/infra/repo/user.py
@@ -1,7 +1,7 @@
 from uuid import UUID
 
 from sqlalchemy import select
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.models.user import User
 
@@ -10,15 +10,15 @@ class UserRepo:
     def __init__(self, model):
         self.model = model
 
-    async def get(self, db: Session, /, id: UUID) -> User | None:
+    async def get(self, db: AsyncSession, /, id: UUID) -> User | None:
         return (await db.execute(select(self.model).filter_by(id=id))).scalar()
 
-    async def get_by_username(self, db: Session, /, username: str) -> User | None:
+    async def get_by_username(self, db: AsyncSession, /, username: str) -> User | None:
         return (
             await db.execute(select(self.model).filter_by(username=username))
         ).scalar()
 
-    async def get_by_email(self, db: Session, /, email: str) -> User | None:
+    async def get_by_email(self, db: AsyncSession, /, email: str) -> User | None:
         return (await db.execute(select(self.model).filter_by(email=email))).scalar()
 
 
